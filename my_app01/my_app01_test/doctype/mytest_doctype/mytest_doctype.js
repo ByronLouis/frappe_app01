0f06b3755ba1f51c5c2668c9017cd331629edefa
ByronLouis <122530297+ByronLouis@users.noreply.github.com>
2024-10-11 09:24:45 +0000

    first commit

---
+++ my_app01/my_app01_test/doctype/mytest_doctype/mytest_doctype.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Byron Louis A. Rabajante and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("MyTest DocType", {
+// 	refresh(frm) {
+
+// 	},
+// });
